9b1ccec8a57d6900c3eb32630fad1de543691a96
franmassello <franmassello@gmail.com>
2022-02-23 12:43:35 -0300

    add first stage route done

---
+++ main.py
@@ -0,0 +1,25 @@
+# pip install Flask 
+
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+def gcd_euclides(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd_euclides(b, a % b)
+
+@app.route('/', methods=['GET'])
+def home():
+    variableTest = "Hello World"
+    return variableTest
+
+@app.route('/gcd', methods=['GET'])
+def gcd():
+    variableA = int(request.args.get('a'))
+    variableB = int(request.args.get('b'))
+    variableResult = gcd_euclides(variableA, variableB)
+    return jsonify(variableResult)
+
+app.run()
